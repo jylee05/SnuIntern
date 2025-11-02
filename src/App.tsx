92785ab6292f03437094c47e765c33fee3999ea7
Seoyeon Heo <161552352+h-seo-n@users.noreply.github.com>
2025-11-02 15:36:29 +0900

    add features (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Topbar from './components/Topbar';
 import { AuthProvider } from './contexts/AuthContext';
+import { PostProvider } from './contexts/PostContext';
 
 const App = () => (
   <Router>
     <AuthProvider>
-      <Topbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </main>
+      <PostProvider>
+        <Topbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+      </PostProvider>
     </AuthProvider>
   </Router>
 );
